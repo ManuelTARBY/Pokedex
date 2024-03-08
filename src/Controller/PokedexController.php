59c23e898e10cae294d7c29cfe5694255910dfa6
Manuel Tarby <manuel.tarby@gmail.com>
2024-03-08 21:17:34 +0100

    Création de la requête pour la page Pokedex et remplissage de la page Pokedex sous forme de tableau

--- src/Controller/PokedexController.php
+++ src/Controller/PokedexController.php
@@ -5,14 +5,28 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Doctrine\ORM\EntityManagerInterface;
 
 class PokedexController extends AbstractController
 {
     #[Route('/pokedex', name: 'app_pokedex')]
-    public function index(): Response
+    public function index(EntityManagerInterface $entityManager): Response
     {
+        $connection = $entityManager->getConnection();
+
+        $sql = '
+            SELECT pokemon.pokedex_id, pokemon.name_fr, GROUP_CONCAT(type.logo) AS logos
+            FROM type_pokemon
+            JOIN pokemon ON pokemon.id = type_pokemon.pokemon_id
+            JOIN type ON type.id = type_pokemon.type_id
+            GROUP BY pokemon.pokedex_id, pokemon.name_fr
+        ';
+
+        $statement = $connection->executeQuery($sql);
+        $results = $statement->fetchAllAssociative();
+
         return $this->render('pokedex/index.html.twig', [
-            'controller_name' => 'PokedexController',
+            'results' => $results,
         ]);
     }
 }
