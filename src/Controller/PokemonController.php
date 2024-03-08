47bb9397b75e2932236e787adbc8f1a7a3702c54
Manuel Tarby <manuel.tarby@gmail.com>
2024-03-08 22:24:41 +0100

    Création de la requête pour récupérer les données à afficher dans la page pokemon et affichage des données dans cette page (en cours)

--- src/Controller/PokemonController.php
+++ src/Controller/PokemonController.php
@@ -5,14 +5,32 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Entity\Pokemon;
+use Doctrine\ORM\EntityManagerInterface;
 
 class PokemonController extends AbstractController
 {
     #[Route('/pokemon/{pokemon_id}', name: 'app_pokemon')]
-    public function index($pokemon_id): Response
+    public function index(EntityManagerInterface $entityManager, $pokemon_id): Response
     {
+        $connection = $entityManager->getConnection();
+
+        $sql = '
+            SELECT pokemon.*, generation.number AS "gene"
+            FROM pokemon
+            JOIN generation on generation.id = pokemon.generation_id
+            WHERE pokedex_id =
+        ' . $pokemon_id;
+
+        $statement = $connection->executeQuery($sql);
+        $result = $statement->fetchAssociative();
+
         return $this->render('pokemon/index.html.twig', [
-            'controller_name' => 'PokemonController',
+            'pokemon' => $result,
         ]);
+
+        // return $this->render('pokemon/index.html.twig', [
+        //     'controller_name' => 'PokemonController',
+        // ]);
     }
 }
