f459693adffa1b39cd7949262ba6283a64892600
Manuel Tarby <manuel.tarby@gmail.com>
2024-04-08 13:44:51 +0200

    Finalisation de la page pokemon avec lien des prévolutions et évolutions

--- src/Controller/PokemonController.php
+++ src/Controller/PokemonController.php
@@ -3,7 +3,6 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Doctrine\ORM\EntityManagerInterface;
@@ -36,11 +35,27 @@ class PokemonController extends AbstractController
         $statement = $connection->executeQuery($sql);
         $resultTypes = $statement->fetchAllAssociative();
 
-        $types_json = new JsonResponse($resultTypes);
+        $sql = 'SELECT pokemon.name_fr, pokemon.pokedex_id FROM pokemon 
+        JOIN pokemon_pokemon ON pokemon.id = pokemon_pokemon.pokemon_target
+        WHERE pokemon_pokemon.pokemon_source = (SELECT pokemon.id 
+        FROM pokemon WHERE pokemon.pokedex_id = ' . $pokemon_id . ')';
+
+        $statement = $connection->executeQuery($sql);
+        $resultEvo = $statement->fetchAllAssociative();
+
+        $sql = 'SELECT pokemon.name_fr, pokemon.pokedex_id FROM pokemon 
+        JOIN pokemon_pokemon ON pokemon.id = pokemon_pokemon.pokemon_source
+        WHERE pokemon_pokemon.pokemon_target = (SELECT pokemon.id
+        FROM pokemon WHERE pokemon.pokedex_id = ' . $pokemon_id . ')';
+
+        $statement = $connection->executeQuery($sql);
+        $resultPrevo = $statement->fetchAllAssociative();
 
         return $this->render('pokemon/index.html.twig', [
             'pokemon' => $resultPoke,
             'types' => $resultTypes,
+            'evolutions' => $resultEvo,
+            'prevolutions' => $resultPrevo,
         ]);
     }
 }
