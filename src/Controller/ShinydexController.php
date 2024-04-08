5df241018ff313c09b2eee1d72285d584782271a
Terry GYSELINGS <terrygyselings@MacBook-Air-de-Terry.local>
2024-04-08 10:13:51 +0200

    authentificateur

--- src/Controller/ShinydexController.php
+++ src/Controller/ShinydexController.php
@@ -3,16 +3,44 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Doctrine\ORM\EntityManagerInterface;
 
 class ShinydexController extends AbstractController
 {
+    private $entityManager;
+    public function __construct(EntityManagerInterface $entityManager){
+        $this->entityManager = $entityManager;
+    }
+
     #[Route('/shinydex', name: 'app_shinydex')]
     public function index(): Response
     {
+        $connection = $this->entityManager->getConnection();
+
+        $sql = '
+            SELECT pokedex_id, sprite_shiny
+            FROM pokemon
+            ORDER BY pokedex_id
+        ';
+
+        $statement = $connection->executeQuery($sql);
+        $resultPoke = $statement->fetchAssociative();
+
+        $sql = '
+            SELECT * 
+            FROM generation 
+            ORDER BY number
+        ';
+
+        $statement = $connection->executeQuery($sql);
+        $generations = $statement->fetchAllAssociative();
+
         return $this->render('shinydex/index.html.twig', [
-            'controller_name' => 'ShinydexController',
+            'pokemons' => $resultPoke,
+            'generations' => $generations,
         ]);
     }
 }
