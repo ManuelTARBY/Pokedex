31042ebe94153eb5877f52df8ea3563b8b90030a
Manuel Tarby <manuel.tarby@gmail.com>
2024-04-10 09:06:19 +0200

    Finalisation page pokemon

--- src/Controller/PokemonController.php
+++ src/Controller/PokemonController.php
@@ -3,44 +3,37 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Doctrine\ORM\EntityManagerInterface;
+use App\Entity\Pokemon;
 
 class PokemonController extends AbstractController
 {
+    private EntityManagerInterface $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager) {
+        $this->entityManager = $entityManager;
+    }
+    
     #[Route('/pokemon/{pokemon_id}', name: 'app_pokemon', methods: ['GET'])]
-    public function index(EntityManagerInterface $entityManager, $pokemon_id): Response
+    public function index($pokemon_id): Response
     {
-        $connection = $entityManager->getConnection();
+        $connection = $this->entityManager->getConnection();
 
-        $sql = 'SELECT pokemon.*, generation.number AS "gene", type.logo AS "logo"
-        FROM pokemon
-        JOIN generation ON generation.id = pokemon.generation_id
-        JOIN type_pokemon ON pokemon.id = type_pokemon.pokemon_id
-        JOIN type ON type.id = type_pokemon.type_id
-        WHERE pokedex_id =
-        ' . $pokemon_id;
+        $sql = 'SELECT pokemon.name_fr, pokemon.pokedex_id FROM pokemon 
+        JOIN pokemon_pokemon ON pokemon.id = pokemon_pokemon.pokemon_source
+        WHERE pokemon_pokemon.pokemon_target = (SELECT pokemon.id
+        FROM pokemon WHERE pokemon.pokedex_id = ' . $pokemon_id . ')';
 
         $statement = $connection->executeQuery($sql);
-        $resultPoke = $statement->fetchAssociative();
+        $resultPrevo = $statement->fetchAllAssociative();
 
-        $sql = 'SELECT type.logo
-        FROM type
-        JOIN type_pokemon ON type_pokemon.type_id = type.id
-        JOIN pokemon ON pokemon.id = type_pokemon.pokemon_id
-        WHERE pokemon.pokedex_id = 
-        ' . $pokemon_id;
-
-        $statement = $connection->executeQuery($sql);
-        $resultTypes = $statement->fetchAllAssociative();
-
-        $types_json = new JsonResponse($resultTypes);
+        $pokemon = $this->entityManager->getRepository(Pokemon::class)->findOneBy(['pokedex_id' => $pokemon_id]);
 
         return $this->render('pokemon/index.html.twig', [
-            'pokemon' => $resultPoke,
-            'types' => $resultTypes,
+            'pokemon' => $pokemon,
+            'prevolutions' => $resultPrevo,
         ]);
     }
 }
