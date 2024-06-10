00721ba10d895bc84803855b0b9ec7e4035759bd
TerryGyselings <terry.gyselings@viacesi.fr>
2024-06-10 10:40:43 +0200

    message erreur connexion

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -31,7 +31,7 @@ class RegistrationController extends AbstractController
                     $form->get('password')->getData()
                 )
             );
-
+            
             $entityManager->persist($user);
             $entityManager->flush();
 
